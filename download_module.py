6cb6228eda7886a4efe456b42725c45e2aa56bfe
mokshayagna <135941753+mokshayagna@users.noreply.github.com>
2024-10-22 19:24:06 +0530

    module

---
+++ download_module.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+def download_files(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        folder_name = "downloaded_files"  # Fixed folder name typo
+        os.makedirs(folder_name, exist_ok=True)
+        print(f"Folder '{folder_name}' is created or already exists.")
+        
+        url_part = url.split("/")[-1].split("?")[0]
+        file_name = f"{url_part}.pdf"
+        file_path = os.path.join(folder_name, file_name)
+
+        if os.path.exists(file_path):
+            print(f"'{file_name}' already exists in the folder")
+        else:
+            with open(file_path, "wb") as file:  
+                file.write(response.content)  
+            print(f"PDF '{file_name}' downloaded successfully!")
+    else:
+        print(f"Failed to download from {url}, status code: {response.status_code}")
